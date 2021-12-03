8a0667b53ce9689e62cfdacc8cd5a050a47269b3
Gomathi K <67832802+kgomathi2910@users.noreply.github.com>
2021-12-04 00:03:14 +0530

    Add files via upload

---
+++ src/electricity/Conn.java
@@ -0,0 +1,18 @@
+package electricity;
+
+import java.sql.*;  
+
+public class Conn {
+    Connection c;
+    Statement s;
+    
+    public Conn() {  
+        try {  
+            Class.forName("com.mysql.cj.jdbc.Driver");  
+            c = DriverManager.getConnection("jdbc:mysql:///project","root","");    
+            s = c.createStatement();         
+        } catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
